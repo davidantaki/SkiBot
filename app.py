845ca9ade52dec070125fb92adfe663b5ac8052d
antakidavid@gmail.com <antakidavid@gmail.com>
2021-09-01 21:41:34 -0400

    Add wax robot move()

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # Flask app backend
 
 from flask import Flask
+import waxing_control
 
+waxing_control.setup()
 
 app = Flask(__name__)
 
@@ -9,7 +11,12 @@ app = Flask(__name__)
 def index():
     return "<p>Hello, World!</p>"
 
-
+@app.route("/<moveDir>")
+def move(moveDir):
+    if moveDir == "moveleft":
+        waxing_control.moveLeft
+    if moveDir == "moveright":
+        waxing_control.moveRight
 
 if __name__ == '__main__':
     app.run(debug=False, port=5000, host='0.0.0.0')
