a555c6cf5cfcded736d994312f4f75eeb8253d26
David Antaki <antakidavid@gmail.com>
2021-09-02 02:57:12 +0100

    Render with buttons

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Flask app backend
 
-from flask import Flask
+from flask import Flask, render_template
 import waxing_control
 
 waxing_control.setup()
@@ -9,14 +9,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 @app.route("/<moveDir>")
 def move(moveDir):
     if moveDir == "moveleft":
-        waxing_control.moveLeft
+        waxing_control.moveLeft()
     if moveDir == "moveright":
-        waxing_control.moveRight
+        waxing_control.moveRight()
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=False, port=5000, host='0.0.0.0')
