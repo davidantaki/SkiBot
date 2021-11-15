6f3d85f88e77db6dd6d16709235d35eab6d3a410
David Antaki <antakidavid@gmail.com>
2021-11-15 04:23:39 +0000

    Add vcsel sensors to the waxing control
    (cherry picked from commit 6d68d5bed0822b2651cae1b65a37b642749688d1)
    Signed-off-by: David Antaki <antakidavid@gmail.com>

--- waxing_control.py
+++ waxing_control.py
@@ -3,34 +3,61 @@
 
 import RPi.GPIO as gpio
 import time
+import busio
+import adafruit_vl6180x
+
+# BOARD Pin definitions
+# SCL1 = 5
+# SDA1 = 3
+# M1Apin = 10
+# M1Bpin = 12
+# M2Apin = 7
+# M2Bpin = 8
+
+# BCM Pin definitions
+SCL1 = 3
+SDA1 = 2
+M1Apin = 15
+M1Bpin = 18
+M2Apin = 4
+M2Bpin = 14
+
+i2c1Bus = None
 
 def setup():
+    gpio.cleanup()
+
     freq = 10000
-    gpio.setmode(gpio.BOARD)
+    gpio.setmode(gpio.BCM)
 
     # motor1
-    gpio.setup(10, gpio.OUT, initial=gpio.LOW)   #M1A
-    gpio.setup(12, gpio.OUT, initial=gpio.LOW)	#M1B
+    gpio.setup(M1Apin, gpio.OUT, initial=gpio.LOW)   #M1A
+    gpio.setup(M1Bpin, gpio.OUT, initial=gpio.LOW)	#M1B
 
     # motor2
-    gpio.setup(7, gpio.OUT, initial=gpio.LOW)	#M2A
-    gpio.setup(8, gpio.OUT, initial=gpio.LOW)	#M2B
+    gpio.setup(M2Apin, gpio.OUT, initial=gpio.LOW)	#M2A
+    gpio.setup(M2Bpin, gpio.OUT, initial=gpio.LOW)	#M2B
+
+    # Vcsel sensors
+    global i2c1Bus 
+    i2c1Bus = busio.I2C(SCL1, SDA1)
+    
 
 def teardown():
     gpio.cleanup()
 
 
 def moveLeft():
-    gpio.output(5, gpio.LOW)
-    gpio.output(8, gpio.LOW)
-    gpio.output(3, gpio.HIGH)
-    gpio.output(7, gpio.HIGH)
+    gpio.output(M1Bpin, gpio.LOW)
+    gpio.output(M2Bpin, gpio.LOW)
+    gpio.output(M1Apin, gpio.HIGH)
+    gpio.output(M2Apin, gpio.HIGH)
 
 def moveRight():
-    gpio.output(3, gpio.LOW)
-    gpio.output(7, gpio.LOW)    
-    gpio.output(5, gpio.HIGH)
-    gpio.output(8, gpio.HIGH)
+    gpio.output(M1Bpin, gpio.HIGH)
+    gpio.output(M2Bpin, gpio.HIGH)    
+    gpio.output(M1Apin, gpio.LOW)
+    gpio.output(M2Apin, gpio.LOW)
 
 def moveStop():
     gpio.output(3, gpio.LOW)
@@ -38,22 +65,44 @@ def moveStop():
     gpio.output(5, gpio.LOW)
     gpio.output(8, gpio.LOW)
 
+
+
+def moveAuto(i2c1Bus):
+    # Create sensor instance.
+    vcselLeft = adafruit_vl6180x.VL6180X(i2c1Bus, 0x29)
+    # vcselRight._write_8(0x212, 0x29)
+    vcselRight = adafruit_vl6180x.VL6180X(i2c1Bus, 0x28)
+    
+    moveLeft()
+
+    # main loop
+    while True:
+        # Read the range in millimeters and print it.
+        vcselRightRange_mm = vcselRight.range
+        print("vcselRightRange_mm: {0}mm".format(vcselRightRange_mm), end='')
+        vcselLeftRange_mm = vcselLeft.range
+        print("\t\tvcselRightRange_mm: {0}mm".format(vcselLeftRange_mm))
+
+        if vcselRightRange_mm > 150:
+            moveLeft()
+        elif vcselLeftRange_mm > 120:
+            moveRight()
+        time.sleep(0.1)
+
 def main():
     print("Running...\n")
     setup()
     print("setup() done\n")
 
-    moveLeft()
-    time.sleep(4)
-    moveRight()
-    time.sleep(4)
-    
-    print("tearing down...\n")
-    teardown()
-    print("teardown() down\n")
+    moveAuto(i2c1Bus)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        teardown()
+        exit(0)
 
 
